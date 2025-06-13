515d01c7a76c95d2d2ec03d403f4fbe6b5de18a0
Charitha1507 <saico6562@gmail.com>
2025-06-14 00:09:56 +0530

    First 1

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ Currently, two official plugins are available:
 ## Expanding the ESLint configuration
 
 If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+export const 
+
+GPT_KEY="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";
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,14 +1,13 @@
-// export const API_OPTIONS = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxN2RlMjlmYzkyMDUzYTgzMjVkYjY5NzZkZWY5ODcyNiIsIm5iZiI6MTc0OTE5NTcwNy4wNDksInN1YiI6IjY4NDI5YmJiZjEzZGM5NmVhYmFkOTU4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BykKEyJEzkMizsut2wzuTPc8X3giXijIn-yiX9-jqwk'
-//   }
-// }
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxN2RlMjlmYzkyMDUzYTgzMjVkYjY5NzZkZWY5ODcyNiIsIm5iZiI6MTc0OTE5NTcwNy4wNDksInN1YiI6IjY4NDI5YmJiZjEzZGM5NmVhYmFkOTU4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BykKEyJEzkMizsut2wzuTPc8X3giXijIn-yiX9-jqwk'
+  }
+}
 
 export const BG_URL="https://assets.nflxext.com/ffe/siteui/vlv3/af2fac72-d956-4952-8686-4d45d359d78c/web/IN-en-20250526-TRIFECTA-perspective_5db3e163-56f7-47c7-9a65-b79b9d76bf24_small.jpg";
 export const MOVIE_URL = 'https://api.themoviedb.org/3/movie/now_playing?page=1';
 export const NETFLIX_LOGO_URL = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const PROFILE_LOGO="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg";
 export const GENRE_URL="https://api.themoviedb.org/3/genre/movie/list";
-//export const GPT_KEY="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";
